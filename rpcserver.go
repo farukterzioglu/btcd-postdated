ae624ac58d303e7817044b5986d76800d2858ebb
Faruk Terzioğlu <faruk.terzioglu@hotmail.com>
2018-10-04 21:47:10 +0300

    rpcserver: Add transfertransaction JSON-RPC command

--- rpcserver.go
+++ rpcserver.go
@@ -214,6 +214,7 @@ var rpcAskWallet = map[string]struct{}{
 	"sendfrom":               {},
 	"sendmany":               {},
 	"sendtoaddress":          {},
+	"transfertransaction":    {},
 	"setaccount":             {},
 	"settxfee":               {},
 	"signmessage":            {},
@@ -4281,7 +4282,7 @@ func newRPCServer(config *rpcserverConfig) (*rpcServer, error) {
 		gbtWorkState:           newGbtWorkState(config.TimeSource),
 		helpCacher:             newHelpCacher(),
 		requestProcessShutdown: make(chan struct{}),
-		quit: make(chan int),
+		quit:                   make(chan int),
 	}
 	if cfg.RPCUser != "" && cfg.RPCPass != "" {
 		login := cfg.RPCUser + ":" + cfg.RPCPass
